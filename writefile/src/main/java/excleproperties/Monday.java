f09793e26413bb5184a06d9a860fd41c3983d04d
Emad <emaduddinaksir@gmail.com>
2023-08-05 20:38:46 +0600

    automation

---
+++ writefile/src/main/java/excleproperties/Monday.java
@@ -0,0 +1,45 @@
+package excleproperties;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.testng.annotations.Test;
+
+public class Monday {
+	@Test
+	public void test() throws IOException{
+	    String excelFilePath = "C:\\Users\\EMAD\\eclipse-workspace\\writefile\\src\\main\\java\\datafiles\\Excel.xlsx";
+	    FileInputStream inputStream = new FileInputStream(excelFilePath);
+
+	    XSSFWorkbook workbook = new XSSFWorkbook(inputStream);
+	    XSSFSheet sheet2 = workbook.getSheetAt(2);
+	    
+	    int rows=sheet2.getLastRowNum();
+	    int cols =sheet2.getRow(1).getLastCellNum();
+
+	    for(int r=2 ; r<=rows;r++)
+	    {
+	        XSSFRow row = sheet2.getRow(r);
+	        for(int c=1; c<cols;c++)
+	        {
+	            XSSFCell cell=row.getCell(c);
+	            switch(cell.getCellType())
+	            {
+	                case STRING: System.out.print(cell.getStringCellValue()); break;
+	                case NUMERIC: System.out.print(cell.getNumericCellValue());break;
+	                case BOOLEAN: System.out.print(cell.getBooleanCellValue());break;
+				default:
+					break;
+
+	            }
+	            System.out.print(" | ");
+	        }
+	        System.out.println();
+	    }
+
+	}
+}
---
+++ writefile/src/main/java/excleproperties/Wednesday.java
@@ -0,0 +1,46 @@
+package excleproperties;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.testng.annotations.Test;
+
+public class Wednesday {
+	@Test
+	public void test2() throws IOException{
+	    String excelFilePath = "C:\\Users\\EMAD\\eclipse-workspace\\writefile\\src\\main\\java\\datafiles\\Excel.xlsx";
+	    FileInputStream inputStream = new FileInputStream(excelFilePath);
+
+	    XSSFWorkbook workbook = new XSSFWorkbook(inputStream);
+	    XSSFSheet sheet2 = workbook.getSheetAt(3);
+	    
+	    int rows=sheet2.getLastRowNum();
+	    int cols =sheet2.getRow(1).getLastCellNum();
+
+	    for(int r=2 ; r<=rows;r++)
+	    {
+	        XSSFRow row = sheet2.getRow(r);
+	        for(int c=1; c<cols;c++)
+	        {
+	            XSSFCell cell=row.getCell(c);
+	            switch(cell.getCellType())
+	            {
+	                case STRING: System.out.print(cell.getStringCellValue()); break;
+	                case NUMERIC: System.out.print(cell.getNumericCellValue());break;
+	                case BOOLEAN: System.out.print(cell.getBooleanCellValue());break;
+				default:
+					break;
+
+	            }
+	            System.out.print(" | ");
+	        }
+	        System.out.println();
+	    }
+
+	}
+
+}
